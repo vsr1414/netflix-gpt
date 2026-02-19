7fd034c399a63b78a977b4bdcc44ba92f78f9d33
LOGON\260018322 <260018322@ge.com>
2026-02-19 23:37:24 +0530

    netflix - gpt initial setup

---
+++ project/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="m-10 p-10">
+      <div className="flex justify-center m-auto">
+        <h1 className="text-3xl font-bold underline bg-cyan-50">
+          Netflix GPT - init
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
